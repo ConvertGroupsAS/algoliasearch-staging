0a7e51cff9ada59cdfe7243c1e0a5f351b357cac
Volodymyr Malyk <vladimir@convert.no>
2016-10-22 08:43:35 +0300

    initial commit and module structure

---
+++ Plugin/Helper/Entity/BaseHelper.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author Convert Team
+ * @copyright Copyright (c) 2016 Convert (http://www.convert.no/)
+ */
+namespace Convert\AlgoliaSearchStaging\Plugin\Helper\Entity;
+
+use Magento\Framework\EntityManager\MetadataPool;
+
+class BaseHelper
+{
+
+    protected $metadataPool;
+
+    public function aroundGetIdColumn(
+        \Algolia\AlgoliaSearch\Helper\Entity\BaseHelper $subject,
+        $procede
+    ) {         
+        $metadata = $this->getMetadataPool()->getMetadata(\Magento\Catalog\Api\Data\CategoryInterface::class);
+        $linkField = $metadata->getLinkField();
+    }
+
+    /**
+     * @return \Magento\Framework\EntityManager\MetadataPool
+     */
+    private function getMetadataPool()
+    {
+        if (null === $this->metadataPool) {
+            $this->metadataPool = \Magento\Framework\App\ObjectManager::getInstance()
+                ->get('Magento\Framework\EntityManager\MetadataPool');
+        }
+        return $this->metadataPool;
+    }
+}
